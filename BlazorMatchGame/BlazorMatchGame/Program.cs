3af94b145697001da468410f8f7471c3555443bd
Szigeti Attila <szigetia@collabit.hu>
2020-12-04 19:31:36 +0100

    store best time

--- BlazorMatchGame/BlazorMatchGame/Program.cs
+++ BlazorMatchGame/BlazorMatchGame/Program.cs
@@ -1,3 +1,5 @@
+using Blazored.LocalStorage;
+
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,8 +20,8 @@ namespace BlazorMatchGame
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
+            builder.Services.AddBlazoredLocalStorage();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
             await builder.Build().RunAsync();
         }
     }
